933107360836443a6bf12f9b5aba3c8fe014e5de
Alexis <alexis.paques@gmail.com>
2021-09-10 12:19:11 +0200

    Fix help formatting

--- calculator.py
+++ calculator.py
@@ -152,15 +152,15 @@ class MyClient(discord.Client):
           name="calc ",
           value="""The calc command is used for quick maths with most Python math functions available and floats are handled as Decimals:
 
-Examples:
+**Examples:**
   - calc 0.1 + 0.2 # exact decimals
-    > 0.3
-  - calc 12**23 # large integers
-    > 6624737266949237011120128
+    \\> 0.3
+  - calc 12\\*\\*23 # large integers
+    \\> 6624737266949237011120128
   - calc 1 | 2 # bitwize operations
-    > 6624737266949237011120128
+    \\> 6624737266949237011120128
   - calc sqrt(abs(sin(pi*e))) # python's math functions
-    > 0.8797401237108087""",
+    \\> 0.8797401237108087""",
           inline=False,
       )
 
@@ -169,11 +169,11 @@ Examples:
           value="""The graph command uses pyplot to graph one or multiple functions.
 The functions will be of the type: `y = x` where you only write the right side of it.
 
-Examples:
+**Examples:**
   - graph x
-    > graphs the function `y = x`
+    \\> graphs the function `y = x`
   - graph [x, -x]
-    > graphs the functions `y = x` and `y = -x`""",
+    \\> graphs the functions `y = x` and `y = -x`""",
           inline=False,
       )
 
@@ -182,23 +182,23 @@ Examples:
           value="""This is a basic python execution, but you do not have access to loops.
 Yet, you can execute all string operators and use map, reduce, filter functions.
 
-Examples:
-  - >> "hello".upper()
-    > HELLO
-  - >> list(map(ord, "hello"))
-    >[104, 101, 108, 108, 111]""",
+**Examples:**
+  - \\>\\> "hello".upper()
+    \\> HELLO
+  - \\>\\> list(map(ord, "hello"))
+    \\> [104, 101, 108, 108, 111]""",
           inline=False,
       )
 
       embed.add_field(
           name="conic ",
-          value="""Graphs a 2D conic of type `y**2 + x**2 = 1`.
+          value="""Graphs a 2D conic of type `y\\*\\*2 + x\\*\\*2 = 1`.
 Due to limitations, you have to move all arguments on one side to make it `= 0`.
 
-Examples:
-  - conic x**2 + y**2 - 3**2
+**Examples:**
+  - conic x\\*\\*2 + y\\*\\*2 - 3\\*\\*2
     > makes a circle of radius 3 (x² + y² = 3²)
-  - conic (x**2 + y**2 -1)**3 - x**2 * y**3
+  - conic (x\\*\\*2 + y\\*\\*2 -1)\\*\\*3 - x\\*\\*2 * y\\*\\*3
     > Surprise!""",
           inline=False,
       )
