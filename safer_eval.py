d2d8e58d0b92c68cc5acb020f7b6e4b63ce2e94b
Alexis Paques <alexis.paques@gmail.com>
2021-09-12 20:02:04 +0200

    Let's try again

--- safer_eval.py
+++ safer_eval.py
@@ -5,7 +5,7 @@ TIMEOUT=5
 
 def run_until(seconds: int, func: Callable, *args) -> None:
     with multiprocessing.Pool(processes=5) as pool:
-        result = pool.apply_async(func, [*args])
+        result = pool.apply_async(func, args)
         try:
             return result.get(timeout=seconds)
         except multiprocessing.TimeoutError:
@@ -16,4 +16,4 @@ def slightly_safer_eval(command, globals, locals):
     for name in code.co_names:
         if "_" in name:
             raise NameError(f"Haha, nice try. è_é")
-    return run_until(TIMEOUT, eval, code, globals, locals)
+    return run_until(TIMEOUT, eval, [code, globals, locals])
