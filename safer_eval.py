69c3da4df735a9c4f982d60fbcde1ae93c91b135
Alexis Paques <alexis.paques@gmail.com>
2021-09-12 19:24:26 +0200

    Oups!

--- safer_eval.py
+++ safer_eval.py
@@ -1,7 +1,5 @@
 def slightly_safer_eval(command, globals, locals):
     code = compile(command, "<string>", "eval")
-    print(code.co_names)
-    # Step 3
     for name in code.co_names:
         if "_" in name:
             raise NameError(f"Haha, nice try. è_é")
